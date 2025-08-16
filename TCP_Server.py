85442044eb615f7a4af1214b792b0068874db39e
LuisNava2001 <luisnava2001@gmail.com>
2025-08-16 16:39:36 -0600

    Update TCP_Server.py

--- TCP_Server.py
+++ TCP_Server.py
@@ -1,39 +1,40 @@
 import socket
+import threading
+
+def handle_client(conn, address_client):
+    print(f"[NEW CONNECTION] {address_client}")
+    while True:
+        try:
+            datos = conn.recv(1024).decode().strip()
+            if not datos:
+                break
+            print(f"CLIENT {address_client}: {datos}")
+            if datos.upper() == "DISCONNECT":
+                conn.sendall("CLOSING CONNECTION...".encode())
+                break
+            else:
+                conn.sendall(datos.upper().encode())
+        except ConnectionResetError:
+            print(f"[ERROR] CLIENT {address_client} DISCONNECTED UNEXPECTEDLY.")
+            break
+        except OSError:
+            print(f"[ERROR] CLIENT {address_client} CLOSED THE CONNECTION.")
+            break
+    conn.close()
+    print(f"[CLOSED CONNECTION] {address_client}")
 
 def tcp_server():
-    server_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#   He decidido utilizar IPV4 para este ejemplo con .AF_INET
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(('127.0.0.1', 5000))
-    server_socket.listen(5) #   Coloqué en espera más de un cliente
-    print("===================================================")
-    print("Servidor esperando conexiones en localhost::5000...")
-    print("===================================================\n")
+    server_socket.listen(5)
+    print("========================================================")
+    print("[SERVER] LISTENING ON localhost::5000...")
+    print("========================================================\n")
 
     while True:
-        conn, address_cliente =server_socket.accept()
-        print(f"Conexion Establecida con el Cliente: {address_cliente}")
-
-        while True:
-            try:
-                datos=conn.recv(1024).decode()
-                if not datos:
-                    break
-                print(f"Mensaje recibido del cliente: {datos}")
-                if datos=="DESCONEXION":
-                    print("Se cerro la conexion con el Cliente...")
-                    respuesta="Solicito cerrar la conexion con el Servidor..."
-                    conn.sendall(respuesta.encode())
-                    conn.close()
-                    print("Cerrando...")
-                else:
-                    respuesta=datos.upper()
-                    conn.sendall(respuesta.encode())
-            except ConnectionResetError:
-                print("Cliente desconectado abuptamente...")
-                break
-            except OSError:
-                print("Error: El cliente desconecto abuptamente...")
-                break
-
-        print("Esperando nueva conexion...")
+        conn, address = server_socket.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, address))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS]: {threading.active_count()-1}")
 
 tcp_server()
