ec2c9e4a284a3ed2561d9e7192045bd25d86be63
LuisNava2001 <luisnava2001@gmail.com>
2025-08-20 16:11:52 -0600

    Update TCP_Server.py

--- TCP_Server.py
+++ TCP_Server.py
@@ -1,7 +1,19 @@
 import socket
 import threading
+import logging
+from datetime import datetime
 
-def handle_client(conn, address_client):
+def handle_client(conn, address_client, client_id):
+    log_filename = f"client_{client_id}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+    logger = logging.getLogger(f"Client-{client_id}")
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(log_filename)
+    fh.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+    logger.addHandler(fh)
+
+    logger.info(f"Connection established with {address_client}")
+
     print(f"[NEW CONNECTION] {address_client}")
     while True:
         try:
@@ -9,10 +21,13 @@ def handle_client(conn, address_client):
             if not datos:
                 break
             print(f"CLIENT {address_client}: {datos}")
+            logger.info(f'CLIENT: {datos}')
             if datos.upper() == "DISCONNECT":
                 conn.sendall("CLOSING CONNECTION...".encode())
                 break
-            else:
+            else:   
+                if "como estas" in datos:
+                    datos = "Muy bien, gracias por preguntar, y tu?"
                 conn.sendall(datos.upper().encode())
         except ConnectionResetError:
             print(f"[ERROR] CLIENT {address_client} DISCONNECTED UNEXPECTEDLY.")
@@ -20,6 +35,7 @@ def handle_client(conn, address_client):
         except OSError:
             print(f"[ERROR] CLIENT {address_client} CLOSED THE CONNECTION.")
             break
+    logger.info("CONNECTION CLOSED")
     conn.close()
     print(f"[CLOSED CONNECTION] {address_client}")
 
@@ -31,10 +47,13 @@ def tcp_server():
     print("[SERVER] LISTENING ON localhost::5000...")
     print("========================================================\n")
 
+    client_id = 0
     while True:
         conn, address = server_socket.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, address))
+        client_id += 1
+        thread = threading.Thread(target=handle_client, args=(conn, address, client_id))
         thread.start()
         print(f"[ACTIVE CONNECTIONS]: {threading.active_count()-1}")
 
-tcp_server()
+if __name__ == "__main__":
+    tcp_server()
