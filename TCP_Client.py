55259318a14809c44ab5d45a7e96d0d0309f6e89
LuisNava2001 <luisnava2001@gmail.com>
2025-08-20 16:11:33 -0600

    Update TCP_Client.py

--- TCP_Client.py
+++ TCP_Client.py
@@ -4,18 +4,21 @@ def tcp_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 5000))
     print("==========================================")
-    print("YOU ARE CONNECTED TO THE SERVER TCP...")
+    print("[CLIENT] YOU ARE CONNECTED TO THE SERVER TCP...")
     print("==========================================\n")
 
     while True:
-        mensaje=input("INPUT A MESSAGE TO THE SERVER ('DISCONNECTED' TO CLOSE COMMUNICATION):\n")
+        mensaje=input("INPUT A MESSAGE TO THE SERVER ('DISCONNECT' TO CLOSE COMMUNICATION):\n[CLIENT]: ")
+        if not mensaje.strip():
+            print("Empty message, please try again!")
+            continue
         client_socket.sendall(mensaje.encode())
         if mensaje.strip()=="DISCONNECT":
             client_socket.close()
-            print("CLOSE CONNECTION...")
+            print(f"[CLOSED CONNECTION] {client_socket}")
             print("COMMUNICATION WITH THE SERVER WAS DISCONNECTED....")
             break
         respuesta=client_socket.recv(1024).decode().strip()
-        print(f"SERVER ANSWER: {respuesta}")
+        print(f"[SERVER]: {respuesta}")
 
 tcp_client()
