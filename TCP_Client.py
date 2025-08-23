db0400407f5d5aa698d9fba7a3619f6635bf1174
LuisNava2001 <luisnava2001@gmail.com>
2025-08-23 14:10:48 -0600

    Update TCP_Client.py

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,6 +1,13 @@
 import socket
+import os
+
+def setup_terminal(title="TCP_CLIENT", width=100, height=100):
+    os.system("color 0A")
+    os.system(f"title {title}")
+    os.system(f"mode con: cols={width} lines={height}")
 
 def tcp_client():
+    setup_terminal(title="TCP CLIENT", width=100, height=100)
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1', 5000))
     print("==========================================")
